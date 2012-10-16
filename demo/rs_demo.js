cab0bcacfce3279a262581aeed2d8f496ff84b69
He Lishi <sunikbear@gmail.com>
2012-10-16 22:13:56 +0800

    Add upload with token

--- demo/rs_demo.js
+++ demo/rs_demo.js
@@ -1,58 +1,77 @@
 var qiniu = require('../index.js');
+var mime = require('mime');
 
 qiniu.conf.ACCESS_KEY = '<Please apply your access key>';
 qiniu.conf.SECRET_KEY = '<Dont send your secret key to anyone>';
 
+var key = __filename;
+var friendName = key;
+var bucket = 'qiniu_test_bucket';
+var DEMO_DOMAIN = bucket + '.dn.qbox.me';
+
 var conn = new qiniu.digestauth.Client();
 
-var bucket = 'bucket';
-var key = 'rs_demo.js';
-var friendName = key;
-
-var DEMO_DOMAIN = 'iovip.qbox.me/' + bucket;
+qiniu.rs.mkbucket(conn, bucket, function(resp) {
+  console.log("\n===> Make bucket result: ", resp);
+  if (resp.code != 200) {
+    return;
+  }
+});
 
 var rs = new qiniu.rs.Service(conn, bucket);
 
-rs.drop(function(resp) {
-	console.log("\n===> Drop result: ", resp);
+var scpoe = bucket,
+    expires = 3600,
+    callbackUrl = null,
+    callbackBodyType = "",
+    customer = "sunikbear@gmail.com";
+var token = new qiniu.token.UploadToken(scpoe, expires, callbackUrl, callbackBodyType, customer);
+var uploadToken = token.generateToken();
 
-	rs.putFile(key, null, __filename, function(resp) {
-		console.log("\n===> PutFile result: ", resp);
-		if (resp.code != 200) {
-			return;
-		}
+var mimeType = mime.lookup(__filename);
 
-		rs.putAuth(function(resp) {
-			console.log("\n===> PutAuth result: ", resp);
-			if (resp.code != 200) {
-				return;
-			}
+console.log("Upload Token: ", uploadToken);
+console.log("Mime Type: ", mimeType);
 
-			rs.publish(DEMO_DOMAIN, function(resp) {
-				console.log("\n===> Publish result: ", resp);
-				if (resp.code != 200) {
+rs.drop(function(resp){
+  console.log("\n===> Drop result: ", resp);
+  if (resp.code != 200) {
+    return;
+  }
+
+  var localFile = __filename;
+  var customMeta = "";
+  var callbackParams = {};
+  var enableCrc32Check = false;
+
+  rs.uploadWithToken(uploadToken, __filename, bucket, key, mimeType, customMeta, callbackParams, enableCrc32Check, function(resp){
+    console.log("\n===> Upload File with Token result: ", resp);
+    if (resp.code != 200) {
+      return;
+    }
+    rs.publish(DEMO_DOMAIN, function(resp){
+      console.log("\n===> Publish result: ", resp);
+      if (resp.code != 200){
+        return;
+      }
+      rs.stat(key, function(resp) {
+		  	console.log("\n===> Stat result: ", resp);
+		  	if (resp.code != 200) {
 					return;
 				}
 
-				rs.stat(key, function(resp) {
-					console.log("\n===> Stat result: ", resp);
-					if (resp.code != 200) {
-						return;
-					}
+				rs.get(key, friendName, function(resp) {
+				  console.log("\n===> Get result: ", resp);
+				  if (resp.code != 200) {
+				    return;
+				  }
 
-					rs.get(key, friendName, function(resp) {
-						console.log("\n===> Get result: ", resp);
-						if (resp.code != 200) {
-							return;
-						}
-
-						rs.remove(key, function(resp) {
-							console.log("\n===> Delete result: ", resp);
-						});
-					});
+				  rs.remove(key, function(resp) {
+				    console.log("\n===> Delete result: ", resp);
+				  });
 				});
 			});
-		});
-	});
+    });
+  });
 });
 
