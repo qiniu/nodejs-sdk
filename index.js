e03b10dfbded8cd62213b20a2ea0507b4873abbd
longbai <slongbai@hotmail.com>
2013-07-12 10:21:26 +0800

    delete previous version

--- index.js
+++
@@ -1,9 +0,0 @@
-var libpath = process.env.QINIU_COV ? './lib-cov' : './lib';
-
-module.exports = {
-    conf: require(libpath + '/conf.js'),
-    digestauth: require(libpath + '/digestauth.js'),
-    rs: require(libpath + '/rs.js'),
-    img: require(libpath + '/img.js'),
-    auth: require(libpath + '/auth.js'),
-};
