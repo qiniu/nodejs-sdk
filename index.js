63c3c0b2cd0076bdbb53c46001cba8fb5f969b74
frogcjn <frogcjn@163.com>
2016-07-01 11:06:36 +0800

    export mac in /auth/digest

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var libpath = process.env.QINIU_COV ? './lib-cov' : './qiniu';
 
 module.exports = {
+  auth: {
+    digest: require(libpath + '/auth' + '/digest.js')
+  },
   io: require(libpath + '/io.js'),
   rs: require(libpath + '/rs.js'),
   rsf: require(libpath + '/rsf.js'),
