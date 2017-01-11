8b05c7955177fedf652c238f4368af3b21ad10de
liangchaoboy <liang.chao.boy@gmail.com>
2017-01-12 02:29:07 +0800

    cdnManager

---
+++ qiniu/cdnManager.js
@@ -0,0 +1,141 @@
+var url = require('url');
+var crypto = require('crypto');
+var urllib = require('urllib');
+var util = require('./util');
+var request = require('request');
+
+// 获取域名日志下载链接
+// @link http://developer.qiniu.com/article/fusion/api/log.html
+// domains 域名列表，以英文分号 ; 分割
+
+exports.getCdnLogList = function (domains, logDate){
+    var url = '/v2/tune/log/list\n';
+    var accessToken = util.generateAccessToken(url, '');
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': accessToken,
+    };
+    data = {
+        'day': logDate,
+        'domains': domains
+    }
+
+    req('/v2/tune/log/list', headers, data); 
+}
+
+
+// 获取域名访问流量数据
+// @link http://developer.qiniu.com/article/fusion/api/traffic-bandwidth.html#batch-flux
+exports.getFluxData = function(startDate, endDate, granularity, domain){
+    var url = '/v2/tune/flux\n';
+    var accessToken = util.generateAccessToken(url, '');
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': accessToken,
+    };
+    data = {
+        'startDate': startDate,
+        'endDate': endDate,
+        'granularity': granularity,
+        'domains': domain
+    }
+
+    req('/v2/tune/flux', headers, data); 
+}
+
+
+// 获取域名访问带宽数据
+// @link http://developer.qiniu.com/article/fusion/api/traffic-bandwidth.html
+exports.getBandwidthData = function(startDate, endDate, granularity, domain){
+    var url = '/v2/tune/bandwidth\n';
+    var accessToken = util.generateAccessToken(url, '');
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': accessToken,
+    };
+    data = {
+        'startDate': startDate,
+        'endDate': endDate,
+        'granularity': granularity,
+        'domains': domain
+    }
+
+    req('/v2/tune/bandwidth', headers, data); 
+}
+
+
+// 预取文件链接
+// @link http://developer.qiniu.com/article/fusion/api/prefetch.html
+// 预取urls  prefetchData =  {urls:['http://obbid7qc6.qnssl.com/023']} 
+exports.prefetch =  function(prefetchData){
+    var url = '/v2/tune/prefetch\n';
+    var accessToken = util.generateAccessToken(url, '');
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': accessToken,
+    };
+    var refreshJson = prefetchData;
+    req('/v2/tune/prefetch', headers, prefetchData); 
+}
+
+
+// 刷新链接
+// @link http://developer.qiniu.com/article/fusion/api/refresh.html
+// 刷新urls  refreshDate =  {urls:['http://obbid7qc6.qnssl.com/023']} 
+// 刷新dirs  refreshDate = {dirs:['http://obbid7qc6.qnssl.com/023/']}
+exports.refresh = function(refreshData){
+    var url = '/v2/tune/refresh\n';
+    var accessToken = util.generateAccessToken(url, '');
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': accessToken,
+    };
+    var refreshJson = refreshDate;
+    req('/v2/tune/refresh', headers, refreshDate); 
+}
+
+
+// post 请求
+function req(pahtname, header, datas){
+      urllib.request("http://fusion.qiniuapi.com" + pahtname, {
+      method: 'POST',
+      headers: header,
+      data:datas
+},function (err, data, res) {
+      if (err) {
+        throw err; // you need to handle error 
+      }
+        console.log(res.statusCode);
+        console.log(data.toString())
+
+ });
+}
+
+
+// 构建标准的基于时间戳的防盗链
+// url 链接 格式为：http://user:pass@host.com:8080/2?v=xx
+// encryptKey 时间戳秘钥
+// durationInSeconds 设置有效期，单位秒
+// return 最终的带时间戳防盗链的url
+exports.getAntiLeechAccessUrlBasedOnTimestamp = function(urlString, encryptKey, durationInSeconds){
+    // 获取pathname
+    var urlStr = url.parse(urlString);
+    var pathname = urlStr.pathname;
+
+    //获取linux时间戳（当前时间+有效时间）的16进制
+    var dateNow = parseInt(Date.now()/1000);
+    var timestampNow =  dateNow + durationInSeconds;
+    var expireHex = timestampNow.toString(16);
+
+
+    var signedStr = encryptKey + pathname + expireHex;
+    var md5 = crypto.createHash('md5');
+    var toSignStr = md5.update(signedStr).digest('hex');
+
+    //判断查询参数
+    if(urlStr.search){
+        return urlString + '&sign=' + toSignStr + '&t=' + expireHex;
+    }else{
+        return urlString + '?sign=' + toSignStr + '&t=' + expireHex;
+    }
+}
