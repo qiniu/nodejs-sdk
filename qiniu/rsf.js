9dcc375a88fe9dbe3104e0c92080ff2b243a6e57
jemygraw <jemygraw@gmail.com>
2017-06-22 16:25:29 +0800

    refractor the list prefix function and add example code

--- qiniu/rsf.js
+++ qiniu/rsf.js
@@ -1,40 +1,54 @@
-var rpc = require('./rpc');
-var conf = require('./conf');
-var util = require('./util');
+const rpc = require('./rpc');
+const conf = require('./conf');
+const util = require('./util');
+const querystring = require('querystring');
 
-exports.listPrefix = function(bucket, prefix, marker, limit, delimiter, onret) {
-  var uri = getPrefixUri(bucket, prefix, marker, limit, delimiter);
-  var digest = util.generateAccessToken(uri, null);
+exports.listPrefix = listPrefix;
 
-  rpc.postWithoutForm(uri, digest, onret)
-}
+// 获取指定前缀的文件列表
+// @link https://developer.qiniu.com/kodo/api/1284/list
+//
+// @param bucket 空间名称
+// @param options 列举操作的可选参数
+//                prefix    列举的文件前缀
+//                marker    上一次列举返回的位置标记，作为本次列举的起点信息
+//                limit     每次返回的最大列举文件数量
+//                delimiter 指定目录分隔符
+// @param callbackFunc(err, respBody, respInfo) - 回调函数
+function listPrefix(bucket, options, callbackFunc) {
+  options = options || {};
 
-function getPrefixUri(bucket, prefix, marker, limit, delimiter) {
-  var uri = conf.RSF_HOST + '/' + 'list?' + 'bucket=' + bucket;
-  if (marker) {
-    uri += '&' + 'marker=' + marker;
+  //必须参数
+  var reqParams = {
+    bucket: bucket,
+  };
+
+  if (options.prefix) {
+    reqParams.prefix = options.prefix;
+  } else {
+    reqParams.prefix = "";
   }
 
-  if (limit) {
-    uri += '&' + 'limit=' + limit;
+  if (options.limit >= 1 && options <= 1000) {
+    reqParams.limit = options.limit;
+  } else {
+    reqParams.limit = 1000;
   }
 
-  if (prefix) {
-    uri += '&' + 'prefix=' + prefix;
+  if (options.marker) {
+    reqParams.marker = options.marker;
+  } else {
+    reqParams.marker = "";
   }
 
-  if(delimiter){
-    uri += '&' + 'delimiter=' + delimiter;
+  if (options.delimiter) {
+    reqParams.delimiter = options.delimiter;
+  } else {
+    reqParams.delimiter = "";
   }
+  var reqSpec = querystring.stringify(reqParams);
+  var requestURI = conf.RSF_HOST + '/list?' + reqSpec;
 
-  return uri;
-}
-
-function ListItem(key, hash, fsize, putTime, mimeType, endUser) {
-  this.key = key || null;
-  this.hash = hash || null;
-  this.fsize = fsize || null;
-  this.putTime = putTime || null;
-  this.mimeType = mimeType || null;
-  this.endUser = endUser || null;
+  var auth = util.generateAccessToken(requestURI, null);
+  rpc.postWithForm(requestURI, null, auth, callbackFunc);
 }
