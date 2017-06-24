474c1dcecc93fbbf60cee5c93e1b8fbe303d7f34
jemygraw <jemygraw@gmail.com>
2017-06-25 07:44:32 +0800

    add create upload token example add doc for util.js

---
+++ examples/create_uptoken.js
@@ -0,0 +1,74 @@
+const qiniu = require("../index.js");
+const proc = require("process");
+
+var accessKey = proc.env.QINIU_ACCESS_KEY;
+var secretKey = proc.env.QINIU_SECRET_KEY;
+var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+
+var bucket = 'if-pbl';
+
+//简单上传凭证
+var options = {
+  scope: bucket,
+};
+var putPolicy = new qiniu.rs.PutPolicy(options);
+console.log(putPolicy.uploadToken(mac));
+
+//自定义凭证有效期（示例2小时）
+var options = {
+  scope: bucket,
+  expires: 7200
+}
+var putPolicy = new qiniu.rs.PutPolicy(options);
+console.log(putPolicy.uploadToken(mac));
+
+// 覆盖上传凭证
+var keyToOverwrite = 'qiniu.mp4';
+var options = {
+  scope: bucket + ":" + keyToOverwrite
+}
+var putPolicy = new qiniu.rs.PutPolicy(options);
+console.log(putPolicy.uploadToken(mac));
+
+//自定义上传回复（非callback模式）凭证
+var options = {
+  scope: bucket,
+  returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}'
+}
+var putPolicy = new qiniu.rs.PutPolicy(options);
+console.log(putPolicy.uploadToken(mac));
+
+//带回调业务服务器的凭证（application/json）
+var options = {
+  scope: bucket,
+  callbackUrl: 'http://api.example.com/qiniu/upload/callback',
+  callbackBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
+}
+var putPolicy = new qiniu.rs.PutPolicy(options);
+console.log(putPolicy.uploadToken(mac));
+
+
+//带回调业务服务器的凭证（application/x-www-form-urlencoded）
+var options = {
+  scope: bucket,
+  callbackUrl: 'http://api.example.com/qiniu/upload/callback',
+  callbackBody: 'key=$(key)&hash=$(etag)&bucket=$(bucket)&fsize=$(fsize)&name=$(x:name)'
+}
+var putPolicy = new qiniu.rs.PutPolicy(options);
+console.log(putPolicy.uploadToken(mac));
+
+//带数据处理的凭证
+var saveMp4Entry = qiniu.util.urlsafeBase64Encode(bucket +
+  ":avthumb_test_target.mp4");
+var saveJpgEntry = qiniu.util.urlsafeBase64Encode(bucket +
+  ":vframe_test_target.jpg");
+var avthumbMp4Fop = "avthumb/mp4|saveas/" + saveMp4Entry;
+var vframeJpgFop = "vframe/jpg/offset/1|saveas/" + saveJpgEntry;
+var options = {
+  scope: bucket,
+  persistentOps: avthumbMp4Fop + ";" + vframeJpgFop,
+  persistentPipeline: "video-pipe",
+  persistentNotifyUrl: "http://api.example.com/qiniu/pfop/notify",
+}
+var putPolicy = new qiniu.rs.PutPolicy(options);
+console.log(putPolicy.uploadToken(mac));
--- qiniu/util.js
+++ qiniu/util.js
@@ -1,26 +1,24 @@
-var fs = require('fs');
 var url = require('url');
-var path = require('path');
 var crypto = require('crypto');
 var conf = require('./conf');
 
-exports.isQiniuCallback = isQiniuCallback;
-
-// ------------------------------------------------------------------------------------------
-// func encode
+// Check Timestamp Expired or not
 exports.isTimestampExpired = function(timestamp) {
   return timestamp > parseInt(Date.now() / 1000);
 }
 
+// Encoded Entry
 exports.encodedEntry = function(bucket, key) {
   return exports.urlsafeBase64Encode(bucket + (key ? ':' + key : ''));
 }
 
+// Get accessKey from uptoken
 exports.getAKFromUptoken = function(uploadToken) {
   var sepIndex = uploadToken.indexOf(":");
   return uploadToken.substring(0, sepIndex);
 }
 
+// Get bucket from uptoken
 exports.getBucketFromUptoken = function(uploadToken) {
   var sepIndex = uploadToken.lastIndexOf(":");
   var encodedPutPolicy = uploadToken.substring(sepIndex + 1);
@@ -35,10 +33,6 @@ exports.getBucketFromUptoken = function(uploadToken) {
   }
 }
 
-exports.urlsafeBase64Encode = function(jsonFlags) {
-  var encoded = new Buffer(jsonFlags).toString('base64');
-  return exports.base64ToUrlSafe(encoded);
-}
 
 exports.base64ToUrlSafe = function(v) {
   return v.replace(/\//g, '_').replace(/\+/g, '-');
@@ -48,13 +42,18 @@ exports.urlSafeToBase64 = function(v) {
   return v.replace(/\_/g, '/').replace(/\-/g, '+');
 }
 
-/**
- * UrlSafe Base64 Decode
- */
+// UrlSafe Base64 Decode
+exports.urlsafeBase64Encode = function(jsonFlags) {
+  var encoded = new Buffer(jsonFlags).toString('base64');
+  return exports.base64ToUrlSafe(encoded);
+}
+
+// UrlSafe Base64 Decode
 exports.urlSafeBase64Decode = function(fromStr) {
   return new Buffer(exports.urlSafeToBase64(fromStr), 'base64').toString();
 }
 
+// Hmac-sha1 Crypt 
 exports.hmacSha1 = function(encodedFlags, secretKey) {
   /*
    *return value already encoded with base64
@@ -64,16 +63,18 @@ exports.hmacSha1 = function(encodedFlags, secretKey) {
   return hmac.digest('base64');
 }
 
-// ------------------------------------------------------------------------------------------
-// func generateAccessToken
-
-exports.generateAccessToken = function(mac, uri, body) {
-  var u = url.parse(uri);
+// 创建AccessToken凭证
+// @param mac         AK&SK对象
+// @param requestURI 回调的URL中的requestURI
+// @param reqBody    请求Body，仅当请求的ContentType为
+//                   application/x-www-form-urlencoded时才需要传入该参数
+exports.generateAccessToken = function(mac, requestURI, reqBody) {
+  var u = url.parse(requestURI);
   var path = u.path;
   var access = path + '\n';
 
   if (body) {
-    access += body;
+    access += reqBody;
   }
 
   var digest = exports.hmacSha1(access, mac.secretKey);
@@ -81,7 +82,13 @@ exports.generateAccessToken = function(mac, uri, body) {
   return 'QBox ' + mac.accessKey + ':' + safeDigest;
 }
 
-function isQiniuCallback(mac, path, body, callbackAuth) {
-  var auth = exports.generateAccessToken(mac, path, body);
+// 校验七牛上传回调的Authorization
+// @param mac           AK&SK对象
+// @param requestURI   回调的URL中的requestURI
+// @param reqBody      请求Body，仅当请求的ContentType为
+//                     application/x-www-form-urlencoded时才需要传入该参数
+// @param callbackAuth 回调时请求的Authorization头部值
+exports.isQiniuCallback = function(mac, requestURI, reqBody, callbackAuth) {
+  var auth = exports.generateAccessToken(mac, requestURI, reqBody);
   return auth === callbackAuth;
 }
