4181e60718aa65ac22fa61beed22123d8f02d1aa
jemygraw <jemygraw@gmail.com>
2017-06-24 09:21:37 +0800

    add put file example code to form upload fix lib import problem

--- examples/upload_simple.js
+++ examples/upload_simple.js
@@ -10,9 +10,29 @@ var putPolicy = new qiniu.rs.PutPolicy({
 
 var uploadToken = putPolicy.uploadToken(mac);
 var config = new qiniu.conf.Config();
+var localFile = "/Users/jemy/Documents/github.png";
 //config.zone = qiniu.zone.Zone_z0;
 var formUploader = new qiniu.form_io.FormUploader(config);
+var putExtra = new qiniu.form_io.PutExtra();
+
+
+//bytes
 formUploader.put(uploadToken, null, "hello", null, function(respErr,
+  respBody, respInfo) {
+  if (respErr) {
+    throw respErr;
+  }
+
+  if (respInfo.statusCode == 200) {
+    console.log(respBody);
+  } else {
+    console.log(respInfo.statusCode);
+    console.log(respBody);
+  }
+});
+
+//file
+formUploader.putFile(uploadToken, null, localFile, putExtra, function(respErr,
   respBody, respInfo) {
   if (respErr) {
     throw respErr;
