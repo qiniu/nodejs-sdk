dbd04a020fd1784e2f8e42fc7f29944ae55d8fd1
jemygraw <jemygraw@gmail.com>
2017-06-25 08:08:52 +0800

    add fop test case

--- test/fop.test.js
+++ test/fop.test.js
@@ -1,43 +1,54 @@
 var fop = require('../').fop;
 
-describe('test start step 0', function() {
-  describe('fop.js', function() {
-    var pic = 'http://private-res.qiniudn.com/gogopher.jpg';
-
-    describe('fop.Exif#makeRequest()', function() {
-      it('test makeRequest', function(done) {
-        var exif = new fop.Exif();
-        var returl = exif.makeRequest(pic);
-        returl.should.equal(pic + '?exif');
-        done();
-      });
-    });
-
-    describe('fop.ImageView#makeRequest()', function() {
-      it('test makeRequest of ImageView', function(done) {
-        var iv = new fop.ImageView();
-        iv.height = 100;
-        iv.width = 40;
-        var returl = iv.makeRequest(pic);
-        returl.should.equal(pic + '?imageView2/1/w/40/h/100');
-
-        iv.quality = 20;
-        iv.format = 'jpg';
-        returl = iv.makeRequest(pic);
-        returl.should.equal(pic + '?imageView2/1/w/40/h/100/q/20/format/jpg');
-        done();
-      });
-    });
-
-    describe('fop.ImageInfo#makeRequest()', function() {
-      it('test makeRequest of ImageInfo', function(done) {
-        var ii = new fop.ImageInfo();
-        var returl = ii.makeRequest(pic);
-        returl.should.equal(pic + '?imageInfo');
-        done();
-      });
-    });
-
-  });
+before(function(done) {
+  if (!process.env.QINIU_ACCESS_KEY) {
+    console.log('should run command `source test-env.sh` first\n');
+    process.exit(0);
+  }
+  done();
 });
 
+describe('test start fop', function() {
+  it('test video fop', function() {
+
+    const qiniu = require("../index.js");
+    const proc = require("process");
+
+    var accessKey = proc.env.QINIU_ACCESS_KEY;
+    var secretKey = proc.env.QINIU_SECRET_KEY;
+    var srcBucket = proc.env.QINIU_TEST_BUCKET;
+    var pipeline = 'sdktest';
+    var srcKey = 'qiniu.mp4';
+
+    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+    var config = new qiniu.conf.Config();
+    //config.useHttpsDomain = true;
+    config.zone = qiniu.zone.Zone_z0;
+    var operManager = new qiniu.fop.OperationManager(mac, config);
+
+    //处理指令集合
+    var saveBucket = srcBucket;
+    var fops = [
+      'avthumb/mp4/s/480x320/vb/150k|saveas/' + qiniu.util.urlsafeBase64Encode(
+        saveBucket + ":qiniu_480x320.mp4"),
+      'vframe/jpg/offset/10|saveas/' + qiniu.util.urlsafeBase64Encode(
+        saveBucket +
+        ":qiniu_frame1.jpg")
+    ];
+
+    var options = {
+      'notifyURL': 'http://api.example.com/pfop/callback',
+      'force': false,
+    };
+
+    //持久化数据处理返回的是任务的persistentId，可以根据这个id查询处理状态
+    operManager.pfop(srcBucket, srcKey, fops, pipeline, options,
+      function(err,
+        respBody,
+        respInfo) {
+        should.not.exist(err);
+        ret.should.have.keys('persistentId');
+        done();
+      });
+  });
+});
