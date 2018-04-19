dff426b19f5a5baf08ec53a2ac4e33c8c25a56ea
guhao <guhao@qiniu.com>
2018-04-19 11:38:20 +0800

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var libPath = process.env.QINIU_COV ? './lib-cov' : './qiniu';
-
-module.exports = {
-  auth: {
-    digest: require(libPath + '/auth' + '/digest.js')
-  },
-  cdn: require(libPath + "/cdn.js"),
-  form_up: require(libPath + '/storage/form.js'),
-  resume_up: require(libPath + '/storage/resume.js'),
-  rs: require(libPath + '/storage/rs.js'),
-  fop: require(libPath + '/fop.js'),
-  conf: require(libPath + '/conf.js'),
-  rpc: require(libPath + '/rpc.js'),
-  util: require(libPath + '/util.js'),
-  zone: require(libPath + '/zone.js')
-};
+exports.Credentials = require('./rtc/credentials');
+exports.App = require('./rtc/app')
+exports.Room = require('./rtc/room')
