d94bd4d88af30b086e6a7ed45dd5ec68d4982d2b
liangchaoboy <liang.chao.boy@gmail.com>
2018-06-25 00:33:20 +0800

    add pfops_video_plus.js

---
+++ examples/pfops_video_plus.js
@@ -0,0 +1,64 @@
+var qiniu = require('/Users/liangchao/qiniu/qiniu-sdk/node/node_modules/qiniu/qiniu');
+var request = require('sync-request');
+var urllib = require('urllib');
+
+
+qiniu.conf.ACCESS_KEY = 'ak';
+qiniu.conf.SECRET_KEY = 'sk';
+
+var url = 'http://argus.atlab.ai/v1/video/89999sssss';
+
+var mac = new qiniu.auth.digest.Mac(qiniu.conf.ACCESS_KEY, qiniu.conf.SECRET_KEY);
+
+var json = {
+
+  "data":{
+        "uri": "http:///Videos/2016-09/39/9d019f7acab742ddbc5f4db02b6f72cb.mp4"
+  },
+  "params": {
+        "async": false,
+        "vframe":{
+          "mode":0,
+          "interval":5
+        }
+    },
+  "ops": [
+        {
+            "op":"pulp",
+            "params":{
+              "labels":[
+              {
+                "label":"1",
+                "select":2,
+                "score":0.0002
+              },
+               {
+                "label":"2",
+                "select":2,
+                "score":0.0002
+              },
+
+              ]
+            }
+        }
+    ]
+}
+
+var accessToken = qiniu.util.generateAccessTokenV2(mac, url, 'POST', 'application/json', JSON.stringify(json));
+
+urllib.request(url, {
+  method: 'POST',
+  headers: {
+    'Authorization': accessToken,
+    'Content-Type': 'application/json'
+  },
+  data: JSON.stringify(json)
+},function (err, data, res) {
+  if (err) {
+    throw err; // you need to handle error 
+    console.log(err);
+  }
+  console.log(res.statusCode);
+  console.log(res);
+  console.log(data.toString())
+});
