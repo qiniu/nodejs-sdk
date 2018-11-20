17bd958f25cc82863724a81b2f21eb61fb889c06
JemyCheung <251839954@qq.com>
2018-11-20 23:56:07 +0800

    add cdn test

---
+++ cdntest.js
@@ -0,0 +1,8 @@
+var qiniu = require('./index.js')
+const StringDecoder = require('string_decoder').StringDecoder;
+const decoder = new StringDecoder('utf8');
+var cdnManager = new qiniu.cdn.CdnManager();
+cdnManager.getCdnLogList(['cdn.iorange.vip'],'2018-11-18',(err,respBody,respInfo)=>{
+	var data = JSON.parse(decoder.write(Buffer.from(respInfo.data)));
+	console.log(data.data['cdn.iorange.vip'][0]['url']) ;
+});
