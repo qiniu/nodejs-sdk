40007d2b32aa38f21413fee0a36fcf77abf1a3a6
JemyCheung <zhangwei@qiniu.com>
2018-11-22 14:13:04 +0800

    com

--- cdntest.js
+++ cdntest.js
@@ -2,7 +2,9 @@ var qiniu = require('./index.js')
 const StringDecoder = require('string_decoder').StringDecoder;
 const decoder = new StringDecoder('utf8');
 var cdnManager = new qiniu.cdn.CdnManager();
-cdnManager.getCdnLogList(['cdn.iorange.vip'],'2018-11-18',(err,respBody,respInfo)=>{
+
+cdnManager.getCdnLogList(['cdn.iorange.vip'],'2018-11-18',(err,respBody,
+	respInfo)=>{
 	var data = JSON.parse(decoder.write(Buffer.from(respInfo.data)));
 	console.log(data.data['cdn.iorange.vip'][0]['url']) ;
 });
